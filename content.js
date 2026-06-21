fccf81d5c2040ff964dea7f3743f0e6c4ddab569
theashokx <theashokx@gmail.com>
2026-06-21 21:47:57 +0530

    Done with manipulation of dom

--- content.js
+++ content.js
@@ -1,1 +1,6 @@
-console.log("AI Cursor Assistant Loaded ");
+document.addEventListener("mouseup", () => {
+  const selectedText = window.getSelection().toString().trim();
+  if (selectedText.length > 0) {
+    console.log("selectedText----" + selectedText);
+  }
+});
